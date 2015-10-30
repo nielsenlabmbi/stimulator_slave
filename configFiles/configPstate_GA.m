cfc70477f438ef6b14614adbfaa845764e3b42a4
ephysRigSlave <nielsenlabmbi@gmail.com>
2015-10-30 12:14:08 -0400

    GA parameters updated

--- configFiles/configPstate_GA.m
+++ configFiles/configPstate_GA.m
@@ -1,34 +1,31 @@
 function configPstate_GA
-%periodic grater
+% Initial Pstate configuration for the concatenated spline GA
+% Maintained by Ramanujan Srinath
+% Accepts:
+%   Nothing
+% Returns:
+%   Nothing
 
 global Pstate
 
-Pstate = struct; %clear it
+Pstate = struct;
 
-Pstate.param{1} = {'predelay'  'float'      1       0                'sec'};
-Pstate.param{2} = {'postdelay'  'float'     1       0                'sec'};
-Pstate.param{3} = {'stim_time'  'float'     0.5       0                'sec'};
+Pstate.param{1}     = {'predelay'   'float'     1       0       'sec'};
+Pstate.param{end+1} = {'postdelay'  'float'     1       0       'sec'};
+Pstate.param{end+1} = {'stim_time'  'float'     1       0       'sec'};
 
-Pstate.param{4} = {'x_pos'       'int'      1000       0                'pixels'};
-Pstate.param{5} = {'y_pos'       'int'      700       0                'pixels'};
-Pstate.param{6} = {'xysize'      'float'      5       0                'deg'};
-Pstate.param{7} = {'masksize'      'float'      6       0                'deg'};
+Pstate.param{end+1} = {'runNum'     'int'       1       1       ''};
+Pstate.param{end+1} = {'genNum'   	'int'       1       1       ''};
+Pstate.param{end+1} = {'linNum'   	'int'       1       1       ''};
+Pstate.param{end+1} = {'stimNum'  	'int'       1       1       ''};
 
-Pstate.param{8} = {'stimpath'      'string'     '/Users/NielsenLab/Documents/MATLAB/project2PGA/stim'       1                ''};
-Pstate.param{9} = {'GArun'      'int'     1       1                ''};
-Pstate.param{10} = {'genNum'      'int'     1       1                ''};
-Pstate.param{11} = {'linNum'      'int'     1       1                ''};
-Pstate.param{12} = {'stimnr'      'int'     1       1                ''};
+Pstate.param{end+1} = {'fore_r'     'float'   	1       0      	''};
+Pstate.param{end+1} = {'fore_g'    	'float'   	1       0    	''};
+Pstate.param{end+1} = {'fore_b'  	'float'   	1       0     	''};
 
-Pstate.param{13} = {'useBubbles'      'int'     0       1                ''};
-Pstate.param{14} = {'numBestStimBubbles'      'int'     4       1                ''};
+Pstate.param{end+1} = {'maskColor' 	'float'     0.5  	0     	''};
+Pstate.param{end+1} = {'background' 'float'     0       0    	''};
+Pstate.param{end+1} = {'contrast'   'float'     100   	0     	'%'};
 
-Pstate.param{15} = {'fore_r'      'float'      1       0                ''};
-Pstate.param{16} = {'fore_g'      'float'      1       0                ''};
-Pstate.param{17} = {'fore_b'      'float'      1       0                ''};
-
-Pstate.param{18} = {'maskcolor'      'float'   0.5       0                ''};
-Pstate.param{19} = {'background'      'float'   0       0                ''};
-Pstate.param{20} = {'contrast' 'float'   100       0             '%'};
-
-
+Pstate.param{end+1} = {'stimPath'   'string'  	'/Volumes/NielsenHome/Matlab/GA/projectGA/stim/'       1                ''};
+Pstate.param{end+1} = {'respPath'   'string'  	'/Volumes/NielsenHome/Matlab/GA/projectGA/resp/'       1                ''};
