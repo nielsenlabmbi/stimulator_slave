075495645bb772796e817be190e2d87b93166643
Kristina <nielsenlabmbi@gmail.com>
2016-07-10 21:38:45 -0400

    first transparent plaid version

---
+++ configFiles/configPstate_TransparentPlaid.m
@@ -0,0 +1,39 @@
+function configPstate_TransparentPlaid
+%periodic grater
+
+global Pstate
+
+Pstate = struct; %clear it
+
+Pstate.param{1} = {'predelay'  'float'      2       0                'sec'};
+Pstate.param{2} = {'postdelay'  'float'     2       0                'sec'};
+Pstate.param{3} = {'stim_time'  'float'     1       0                'sec'};
+
+Pstate.param{4} = {'x_pos'       'int'      600       0                'pixels'};
+Pstate.param{5} = {'y_pos'       'int'      400       0                'pixels'};
+Pstate.param{6} = {'x_size'      'float'      3       1                'deg'};
+Pstate.param{7} = {'y_size'      'float'      3       1                'deg'};
+Pstate.param{8} = {'mask_type'   'string'   'none'       0                ''};
+Pstate.param{9} = {'mask_radius' 'float'      6       1                'deg'};
+
+Pstate.param{10} = {'contrast'    'float'     100       0                '%'};
+Pstate.param{11} = {'ori'         'int'        0       0                'deg'};
+Pstate.param{12} = {'s_freq'      'float'      1      -1                 'cyc/deg'};
+Pstate.param{13} = {'s_duty'      'float'   0.5       0                ''};
+Pstate.param{14} = {'t_period'    'int'       20       0                'frames'};
+
+Pstate.param{15} = {'contrast2'    'float'     10       0                '%'};
+Pstate.param{16} = {'ori2'         'int'        90       0                'deg'};
+Pstate.param{17} = {'phase2'         'float'        0       0                'deg'};
+Pstate.param{18} = {'s_freq2'      'float'      1      -1                 'cyc/deg'};
+Pstate.param{19} = {'s_duty2'      'float'   0.5       0                ''};
+
+Pstate.param{20} = {'lum_intersect'      'float'   0       0                ''};
+
+Pstate.param{21} = {'Leye_bit'    'int'   1       0                ''};
+Pstate.param{22} = {'Reye_bit'    'int'   1       0                ''};
+
+
+
+
+
