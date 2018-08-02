d9866d4112089bfc1b13d2af797df99071fb24cb
ephysRigMaster <nielsenlabmbi@gmail.com>
2018-08-02 14:56:16 -0400

    TestTrial
    Test stimulus for optical imaging

---
+++ configFiles/configPstate_TestTrial.m
@@ -0,0 +1,17 @@
+function configPstate_TestTrial
+global Pstate
+
+Pstate = struct; %clear it
+
+Pstate.param{1} = {'predelay'  'float'      2       0                'sec'};
+Pstate.param{2} = {'postdelay'  'float'     2       0                'sec'};
+Pstate.param{3} = {'stim_time'  'float'     1       0                'sec'};
+
+Pstate.param{4} = {'x_pos'       'int'      600       0                'pixels'};
+Pstate.param{5} = {'y_pos'       'int'      400       0                'pixels'};
+Pstate.param{6} = {'x_size'      'float'      3       1                'deg'};
+Pstate.param{7} = {'y_size'      'float'      3       1                'deg'};
+Pstate.param{8} = {'Intensity'      'float'      1       0                ''};
+
+Pstate.param{16} = {'Leye_bit'    'int'   1       0                ''};
+Pstate.param{17} = {'Reye_bit'    'int'   1       0                ''};
