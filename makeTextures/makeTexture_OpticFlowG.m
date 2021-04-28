5361167e0f4eeab0676422cae5cea1c5c0340938
Kristina <nielsenlabmbi@gmail.com>
2021-04-28 16:46:41 -0400

    speed planes

--- makeTextures/makeTexture_OpticFlowG.m
+++ makeTextures/makeTexture_OpticFlowG.m
@@ -46,15 +46,11 @@ xypos=zeros(2,nrDots);
 %displacements: set so that the scaling factor makes the average speed
 %appear at half the stimulus radius
 avgDeltaFrame = deg2pix(P.avgSpeedDots,'none')/fps; %this is the displacement based on the average speed in pixels per frame
-speedScale = 2*avgDeltaFrame/stimRadius; 
+speedScale = 2*avgDeltaFrame/stimRadius;
+speedPlane = [1 1/2 3/2]*avgDeltaFrame; %this is for radius independent speed planes
 
-if P.spiralBit==1 %spiral; everything gets set during first frame
-    deltaFrame=zeros(1,nrDots);
-    dotDir=zeros(1,nrDots);
-else
-    deltaFrame=avgDeltaFrame;
-    dotDir=ones(1,nrDots)*P.ori/180*pi; %needs to be in radians
-end
+deltaFrame=zeros(1,nrDots);
+dotDir=zeros(1,nrDots);
 
 %initialize lifetime vector: set everything to 0 here so every dot gets an
 %initial direction and speed; set to random start value for frame 1 later 
@@ -71,6 +67,7 @@ nrFrames=ceil(P.stim_time*fps);
 DotFrame={};
 
 for f=1:nrFrames 
+        
     %find dots with lifetime 0 (only then does the direction/speed change)
     idx=find(lifetime==0);
     
@@ -84,7 +81,13 @@ for f=1:nrFrames
         %get polar coordinates of dots to figure out speed and direction
         [th,rad]=cart2pol(xypos(1,idx),xypos(2,idx));
         dotDir(idx)=th+P.ori/180*pi; %this is in radians
-        deltaFrame(idx)=speedScale*rad;
+        if P.speedGradient==1
+            deltaFrame(idx)=speedScale*rad;
+        else
+            %split dots into 3 groups to assign to speed planes
+            sidx=randi(s,3,size(idx));
+            deltaFrame(idx)=speedPlane(sidx);
+        end
         
         %for the contracting stimulus, we need to flag dots that would
         %cross to the opposite side of the center and move in the wrong
@@ -94,24 +97,48 @@ for f=1:nrFrames
         %    flagOut=find(deltaFrame>rad);
         %end
     else
-        %for translation, initally reset all of these dots back to signal
-        %direction
-        dotDir(idx)=P.ori/180*pi; 
+        %for translation, set all of these dots to signal direction
+        dotDir(idx)=P.ori/180*pi;
+        if P.speedGradient==1
+            deltaFrame(idx)=avgDeltaFrame;
+        else
+            sidx=randi(s,3,size(idx));
+            deltaFrame(idx)=speedPlane(sidx);
+        end
     end
     
     %noise: either randomly reposition or randomly assign direction to
-    %preserve speed gradient; we do this here for the random direction;
-    %doesn't matter for the random reposition that there is another arbitrary
-    %jump in position after
-    if P.noiseType==0 %reposition, don't do anything about direction/speed
+    %preserve speed gradient
+    if P.noiseType==0 %reposition, assign new speed and direction accordingly
         idxNoise=randperm(s,nrDots,nrNoise);
         rvec=rand(s,[2 length(idxNoise)]); 
         xypos(1,idxNoise)=(rvec(1,:)-0.5)*2*stimRadius;
         xypos(2,idxNoise)=(rvec(2,:)-0.5)*2*stimRadius;
-        lifetime(idxNoise)=1; %these will need a direction to be assigned in the next round
+        
+        if P.spiralBit==1 %spiral           
+            [th,rad]=cart2pol(xypos(1,idxNoise),xypos(2,idxNoise));
+            dotDir(idxNoise)=th+P.ori/180*pi; %this is in radians
+            if P.speedGradient==1
+                deltaFrame(idxNoise)=speedScale*rad;
+            else
+                %split dots into 3 groups to assign to speed planes
+                sidx=randi(s,3,size(idxNoise));
+                deltaFrame(idxNoise)=speedPlane(sidx);
+            end
+        else
+            dotDir(idxNoise)=P.ori/180*pi;
+            if P.speedGradient==1
+                deltaFrame(idxNoise)=avgDeltaFrame;
+            else
+                sidx=randi(s,3,size(idxNoise));
+                deltaFrame(idxNoise)=speedPlane(sidx);
+            end
+        end
+        %reset the lifetime to full (at the end of the loop) 
+        lifetime(idxNoise)=0; 
     else %change direction only
         %we randomly pick a fraction of the lifetime 0 dots for this method
-        %(dots will keep their assignment for their lifetime)        
+        %dots will keep their assignment for their lifetime
         signalid=rand(s,1,length(idx))<P.dotCoherence/100; %id=1: signal
         randdir=round(360*rand(s,1,length(idx)-sum(signalid)));
         dotDir(idx(signalid==0))=randdir;
@@ -121,6 +148,35 @@ for f=1:nrFrames
     xypos(1,:)=xypos(1,:)-deltaFrame.*cos(dotDir);
     xypos(2,:)=xypos(2,:)-deltaFrame.*sin(dotDir);
     
+    %deal with the ones that end up outside of the stimulus: reshuffle and
+    %assign new speed based on position (speed and direction only matter on
+    %next frame)
+    idxOut=find(abs(xypos(1,:))>stimRadius | abs(xypos(2,:))>stimRadius);
+    rvec=rand(s,[2 length(idxOut)]); 
+    xypos(1,idxOut)=(rvec(1,:)-0.5)*2*stimRadius;
+    xypos(2,idxOut)=(rvec(2,:)-0.5)*2*stimRadius;
+    if P.spiralBit==1 %spiral
+        [th,rad]=cart2pol(xypos(1,idxOut),xypos(2,idxOut));
+        dotDir(idxOut)=th+P.ori/180*pi; %this is in radians
+        if P.speedGradient==1
+            deltaFrame(idxOut)=speedScale*rad;
+        else
+            %split dots into 3 groups to assign to speed planes
+            sidx=randi(s,3,size(idxOut));
+            deltaFrame(idxOut)=speedPlane(sidx);
+        end
+    else
+        dotDir(idxOut)=P.ori/180*pi;
+        if P.speedGradient==1
+            deltaFrame(idxOut)=avgDeltaFrame;
+        else
+            sidx=randi(s,3,size(idxOut));
+            deltaFrame(idxOut)=speedPlane(sidx);
+        end
+    end
+    %reset the lifetime to full (at the end of the loop)
+    lifetime(idxOut)=0;
+        
     %update lifetime
     if f==1
         randlife=randi(s,P.dotLifetime,nrDots,1);
@@ -130,16 +186,7 @@ for f=1:nrFrames
         lifetime=lifetime-1;
     end
     
-    %randomly reshuffle the ones that end up outside the stimulus (these
-    %will need to have their speeds etc recomputed, so set their lifetime to 0 at this point)
-    %because density is computed on square, use that to figure out of
-    %bounds
-    idxOut=find(abs(xypos(1,:))>stimRadius | abs(xypos(2,:))>stimRadius);
-    rvec=rand(s,[2 length(idxOut)]); 
-    xypos(1,idxOut)=(rvec(1,:)-0.5)*2*stimRadius;
-    xypos(2,idxOut)=(rvec(2,:)-0.5)*2*stimRadius;
-    lifetime(idxOut)=0;
-    
+   
         
     %for the contracting stimulus only (Sdir==pi), eliminate dots that
     %cross the origin
